4ebff78a3b602b8c75f1b6a15491389a9f1bfdac
MINEDUCYT <MINEDUCYT@ANGEL>
2024-10-27 12:54:19 -0600

    Butoon 1

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace ejemplo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("juan carlos");
+        }
     }
 }
