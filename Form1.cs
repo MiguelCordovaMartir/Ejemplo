a0f0733607d2a99330044551adb5791e78172b93
MINEDUCYT <MINEDUCYT@ANGEL>
2024-10-27 12:57:01 -0600

    buton 2

--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,10 @@ namespace ejemplo
         {
             MessageBox.Show("juan carlos");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("nooooo");
+        }
     }
 }
